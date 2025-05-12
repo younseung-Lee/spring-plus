67d01b734916228f076c1be735ed4587a9e5e92a
younseung Lee <lys5654@cu.ac.kr>
2025-05-12 13:53:31 +0900

    feat: Level 2-7 N + 1

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.comment.repository;
 
 import org.example.expert.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    @EntityGraph(attributePaths = {"user"})
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
